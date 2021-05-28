40ce491f990c7d65fbb9c4e11ceebb79c9b7f310
jaredait <jaredampudia@gmail.com>
2021-05-28 15:27:51 -0500

    Crear estructura clase EstablecimientoMD

---
+++ AdministracionPedidosAlimenticios/src/PAQUETE_MD/EstablecimientoMD.java
@@ -0,0 +1,51 @@
+package PAQUETE_MD;
+
+import PAQUETE_DP.EstablecimientoDP;
+import java.io.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.*;
+
+public class EstablecimientoMD {
+    private EstablecimientoDP establecimientoDP;
+
+    // constructor
+    public EstablecimientoMD(EstablecimientoDP establecimientoDP) {
+        this.establecimientoDP = establecimientoDP;
+    }
+    
+    public boolean insertarMD(){
+        
+        return false;
+    }    
+    
+    public boolean consultarMD(){
+        
+        return false;
+    }
+    
+    public boolean actualizarMD(){
+    
+        return false;
+    }
+    
+    public boolean eliminarMD(){
+        
+        return false;
+    }
+    
+    public boolean verificarExisteMD(){
+        
+        return false;
+    }
+    
+    public ArrayList<EstablecimientoDP> consultarTodosMD(){
+        
+        return null;
+    }
+    
+    public void cargarDatos(){
+        
+    }
+    
+}
