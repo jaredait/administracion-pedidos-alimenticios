d2d219ad6ad9c3acc6efeb5ae524baedec59f137
jaredait <jaredampudia@gmail.com>
2021-05-29 12:44:43 -0500

    Agregar conexion a db MySQL en la nube

--- AdministracionPedidosAlimenticios/src/administracionpedidosalimenticios/AdministracionPedidosAlimenticios.java
+++ AdministracionPedidosAlimenticios/src/administracionpedidosalimenticios/AdministracionPedidosAlimenticios.java
@@ -1,21 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package administracionpedidosalimenticios;
 
-/**
- *
- * @author ASUS
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class AdministracionPedidosAlimenticios {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Conexion a la db
+        AdministracionPedidosAlimenticios conexion = new AdministracionPedidosAlimenticios();
+        Connection connection = null;
+        connection = conexion.get_connection();
+        System.out.println("Conexion: " + connection);
     }
     
+    public Connection get_connection() {
+         Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            //Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://sql10.freesqldatabase.com:3306/sql10415625", "sql10415625", "MEuPnqN9wJ");
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+        return connection;
+    }
 }
