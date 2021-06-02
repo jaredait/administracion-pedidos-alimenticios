70fea2c9e009a1d2b741f9506918dfc785138382
jaredait <jaredampudia@gmail.com>
2021-06-02 17:03:34 -0500

    Agregar metodos clase EntregaMD

--- AdministracionPedidosAlimenticios/src/PAQUETE_GUI/VentanaEntrega_actualizar.java
+++ AdministracionPedidosAlimenticios/src/PAQUETE_GUI/VentanaEntrega_actualizar.java
@@ -5,10 +5,15 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import PAQUETE_DP.EntregaDP;
 
 public class VentanaEntrega_actualizar extends javax.swing.JInternalFrame {
-
+    // atributos
+    EntregaDP entregaDP;
+    
     public VentanaEntrega_actualizar() {
+        entregaDP = new EntregaDP();
+        
         initComponents();
         cargarTiposEntrega();
     }
@@ -35,6 +40,7 @@ public class VentanaEntrega_actualizar extends javax.swing.JInternalFrame {
         cb_actualEntrega_tipo = new javax.swing.JComboBox<>();
         jScrollPane1 = new javax.swing.JScrollPane();
         ta_actualEntrega_comentario = new javax.swing.JTextArea();
+        btn_consulEntrega_guardar = new javax.swing.JButton();
 
         btn_consulEntrega_buscar.setText("Buscar");
 
@@ -55,6 +61,13 @@ public class VentanaEntrega_actualizar extends javax.swing.JInternalFrame {
         ta_actualEntrega_comentario.setColumns(20);
         ta_actualEntrega_comentario.setRows(5);
         jScrollPane1.setViewportView(ta_actualEntrega_comentario);
+
+        btn_consulEntrega_guardar.setText("Guardar");
+        btn_consulEntrega_guardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_consulEntrega_guardarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -77,27 +90,31 @@ public class VentanaEntrega_actualizar extends javax.swing.JInternalFrame {
                         .addGap(18, 18, 18)))
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                        .addComponent(tf_actualEntrega_pedNumero, javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(cb_actualEntrega_tipo, javax.swing.GroupLayout.Alignment.LEADING, 0, 174, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                         .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(tf_actualEntrega_argumento1, javax.swing.GroupLayout.DEFAULT_SIZE, 174, Short.MAX_VALUE)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(tf_actualEntrega_argumento1)
                                 .addComponent(tf_actualEntrega_entNumero))
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGap(18, 18, 18)
                             .addComponent(btn_consulEntrega_buscar))
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(cb_actualEntrega_tipo, javax.swing.GroupLayout.Alignment.LEADING, 0, 196, Short.MAX_VALUE)
+                        .addComponent(tf_actualEntrega_pedNumero, javax.swing.GroupLayout.Alignment.LEADING)))
                 .addContainerGap(172, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(btn_consulEntrega_guardar)
+                .addGap(314, 314, 314))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(51, 51, 51)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(70, 70, 70)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGap(51, 51, 51)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(70, 70, 70)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                     .addComponent(tf_actualEntrega_entNumero, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(jLabel2))
@@ -105,20 +122,27 @@ public class VentanaEntrega_actualizar extends javax.swing.JInternalFrame {
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                     .addComponent(tf_actualEntrega_pedNumero, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(jLabel3))
-                                .addGap(42, 42, 42))
-                            .addComponent(cb_actualEntrega_tipo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(156, 156, 156)
-                        .addComponent(jLabel4))
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(btn_consulEntrega_buscar)
-                        .addComponent(jLabel1)
-                        .addComponent(tf_actualEntrega_argumento1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(18, 18, 18)
+                                .addGap(60, 60, 60))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGap(156, 156, 156)
+                                        .addComponent(jLabel4))
+                                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(btn_consulEntrega_buscar)
+                                        .addComponent(jLabel1)
+                                        .addComponent(tf_actualEntrega_argumento1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addGap(18, 18, 18))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(cb_actualEntrega_tipo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)))
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel5)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(184, Short.MAX_VALUE))
+                .addGap(42, 42, 42)
+                .addComponent(btn_consulEntrega_guardar)
+                .addContainerGap(110, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -135,7 +159,14 @@ public class VentanaEntrega_actualizar extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btn_consulEntrega_guardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_consulEntrega_guardarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn_consulEntrega_guardarActionPerformed
+
     // metodos de la clase
+    
+    public void guardar
+    
     public void cargarTiposEntrega() {
         try {
             Properties props = new Properties();
@@ -152,6 +183,7 @@ public class VentanaEntrega_actualizar extends javax.swing.JInternalFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn_consulEntrega_buscar;
+    private javax.swing.JButton btn_consulEntrega_guardar;
     private javax.swing.JComboBox<String> cb_actualEntrega_tipo;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
